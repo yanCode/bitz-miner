50d5609185471fdd5460fc3ad669b1e0ba9a6c29
Zhang ShengYan <yan.code@gmail.com>
2025-04-14 11:45:36 +0800

    chore: add args file

---
+++ src/args.rs
@@ -0,0 +1,81 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+pub struct AccountArgs {
+    #[arg(value_name = "ADDRESS", help = "The address to the account to fetch.")]
+    pub address: Option<String>,
+
+    #[arg(
+        short,
+        long,
+        value_name = "PROOF_ADDRESS",
+        help = "The address of the proof to fetch."
+    )]
+    pub proof: Option<String>,
+
+    #[command(subcommand)]
+    pub command: Option<AccountCommand>,
+}
+
+#[derive(Subcommand, Clone, Debug)]
+pub enum AccountCommand {
+    #[command(about = "Close an account and reclaim rent.")]
+    Close(AccountCloseArgs),
+}
+
+#[derive(Parser, Clone, Debug)]
+pub struct AccountCloseArgs {}
+
+#[derive(Parser, Debug)]
+pub struct CollectArgs {
+    #[arg(
+        long,
+        short,
+        value_name = "CORES_COUNT",
+        help = "The number of CPU cores to allocate to collecting.",
+        default_value_t = (num_cpus::get() - 1).max(1).to_string()
+    )]
+    pub cores: String,
+
+    #[arg(
+        long,
+        short,
+        value_name = "SECONDS",
+        help = "The number seconds before the deadline to stop collecting and start submitting.",
+        default_value = "5"
+    )]
+    pub buffer_time: u64,
+
+    #[arg(
+        long,
+        short,
+        value_name = "MIN_DIFFICULTY",
+        help = "The minimum difficulty to collect at.",
+        default_value = "20"
+    )]
+    pub min_difficulty: u32,
+
+    #[arg(
+        long,
+        short,
+        value_name = "DEVICE_ID",
+        help = "An optional device id to use for pool collecting (max 5 devices per keypair)."
+    )]
+    pub device_id: Option<u64>,
+
+    #[arg(
+        long,
+        short,
+        value_name = "POOL_URL",
+        help = "The optional pool url to join and forward solutions to."
+    )]
+    pub pool_url: Option<String>,
+
+    #[arg(
+        long,
+        short,
+        help = "Flag indicating whether or not to run in verbose mode.",
+        default_value = "false"
+    )]
+    pub verbose: bool,
+}
