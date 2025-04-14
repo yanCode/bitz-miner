86abe45e255f90243a4943aa5c2fc41fba5250e8
Zhang ShengYan <yan.code@gmail.com>
2025-04-14 08:47:22 +0800

    chore: table

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 mod utils;
 use std::sync::{Arc, RwLock};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use env_logger::Env;
 use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_sdk::commitment_config::CommitmentConfig;
 
 #[tokio::main]
 async fn main() {
     env_logger::init_from_env(Env::default().default_filter_or("info"));
-    let _args = Args::parse();
+    let args = Args::parse();
+    let cli_config = match &args.config_file {
+        Some(config_file) => solana_cli_config::Config::load(config_file).unwrap_or_else(|_| {
+            eprintln!("Failed to load config file: {}", config_file);
+            std::process::exit(1);
+        }),
+        None => {
+            println!(
+                "No config file provided, using default config: {:?}",
+                solana_cli_config::CONFIG_FILE.as_ref().unwrap()
+            );
+            solana_cli_config::Config::default()
+        }
+    };
+    let cluster_url = args.rpc.unwrap_or(cli_config.json_rpc_url);
+    let default_keypair = args.keypair.unwrap_or(cli_config.keypair_path.clone());
+    let fee_payer_filepath = args.fee_payer.unwrap_or(default_keypair.clone());
+    let rpc_client = RpcClient::new_with_commitment(cluster_url, CommitmentConfig::confirmed());
 }
 
 #[derive(Clone)]
@@ -30,7 +48,6 @@ struct Args {
         long,
         value_name = "NETWORK_URL",
         help = "Network address of your RPC provider",
-        default_value = "https://mainnetbeta-rpc.eclipse.xyz/",
         global = true
     )]
     rpc: Option<String>,
@@ -42,4 +59,69 @@ struct Args {
         help = "Filepath to config file."
     )]
     config_file: Option<String>,
+    #[arg(
+        long,
+        value_name = "KEYPAIR_FILEPATH",
+        help = "Filepath to signer keypair. Base58 or Raw JSON.",
+        default_value = "key.txt",
+        global = true
+    )]
+    keypair: Option<String>,
+    #[arg(
+        long,
+        value_name = "FEE_PAYER_FILEPATH",
+        help = "Filepath to transaction fee payer keypair.",
+        global = true
+    )]
+    fee_payer: Option<String>,
+    #[arg(
+        long,
+        value_name = "MICROLAMPORTS",
+        help = "Price to pay for compute units. If dynamic fees are enabled, this value will be used as the cap.",
+        default_value = "1000",
+        global = true
+    )]
+    priority_fee: Option<u64>,
+    #[arg(
+        long,
+        value_name = "DYNAMIC_FEE_URL",
+        help = "RPC URL to use for dynamic fee estimation.",
+        global = true
+    )]
+    dynamic_fee_url: Option<String>,
+    #[arg(long, help = "Enable dynamic priority fees", global = true)]
+    dynamic_fee: bool,
+
+    #[command(subcommand)]
+    command: Commands,
 }
+#[derive(Subcommand, Debug)]
+enum Commands {
+    #[command(about = "Fetch your account details")]
+    Account(AccountArgs),
+}
+#[derive(Parser, Debug)]
+pub struct AccountArgs {
+    #[arg(value_name = "ADDRESS", help = "The address to the account to fetch.")]
+    pub address: Option<String>,
+
+    #[arg(
+        short,
+        long,
+        value_name = "PROOF_ADDRESS",
+        help = "The address of the proof to fetch."
+    )]
+    pub proof: Option<String>,
+
+    #[command(subcommand)]
+    pub command: Option<AccountCommand>,
+}
+
+#[derive(Subcommand, Clone, Debug)]
+pub enum AccountCommand {
+    #[command(about = "Close an account and reclaim rent.")]
+    Close(AccountCloseArgs),
+}
+
+#[derive(Parser, Clone, Debug)]
+pub struct AccountCloseArgs {}
--- src/utils/table.rs
+++ src/utils/table.rs
@@ -1,7 +1,85 @@
+use colored::Colorize;
+use solana_sdk::signature::Signature;
 use tabled::Tabled;
 
 #[derive(Tabled)]
 pub struct TableData {
     pub key: String,
     pub value: String,
+}
+
+#[derive(Clone, Tabled)]
+pub struct SoloCollectingData {
+    #[tabled(rename = "Signature")]
+    pub signature: String,
+    #[tabled(rename = "Block")]
+    pub block: String,
+    #[tabled(rename = "Timestamp")]
+    pub timestamp: String,
+    #[tabled(rename = "Timing")]
+    pub timing: String,
+    #[tabled(rename = "Score")]
+    pub difficulty: String,
+    #[tabled(rename = "Base Reward")]
+    pub base_reward: String,
+    #[tabled(rename = "Boost Reward")]
+    pub boost_reward: String,
+    #[tabled(rename = "Total Reward")]
+    pub total_reward: String,
+    #[tabled(rename = "Status")]
+    pub status: String,
+}
+
+impl SoloCollectingData {
+    pub fn fetching(sig: Signature) -> Self {
+        Self {
+            signature: sig.to_string(),
+            block: "–".to_string(),
+            timestamp: "–".to_string(),
+            difficulty: "–".to_string(),
+            base_reward: "–".to_string(),
+            boost_reward: "–".to_string(),
+            total_reward: "–".to_string(),
+            timing: "–".to_string(),
+            status: "Fetching".to_string(),
+        }
+    }
+
+    pub fn failed() -> Self {
+        Self {
+            signature: "–".to_string(),
+            block: "–".to_string(),
+            timestamp: "–".to_string(),
+            difficulty: "–".to_string(),
+            base_reward: "–".to_string(),
+            boost_reward: "–".to_string(),
+            total_reward: "–".to_string(),
+            timing: "–".to_string(),
+            status: "Failed".bold().red().to_string(),
+        }
+    }
+}
+
+#[derive(Clone, Tabled)]
+pub struct PoolCollectingData {
+    #[tabled(rename = "Signature")]
+    pub signature: String,
+    #[tabled(rename = "Block")]
+    pub block: String,
+    #[tabled(rename = "Timestamp")]
+    pub timestamp: String,
+    #[tabled(rename = "Timing")]
+    pub timing: String,
+    #[tabled(rename = "Score")]
+    pub difficulty: String,
+    #[tabled(rename = "Pool Base Reward")]
+    pub base_reward: String,
+    #[tabled(rename = "Pool Boost Reward")]
+    pub boost_reward: String,
+    #[tabled(rename = "Pool Total Reward")]
+    pub total_reward: String,
+    #[tabled(rename = "My Score")]
+    pub my_difficulty: String,
+    #[tabled(rename = "My Reward")]
+    pub my_reward: String,
 }
