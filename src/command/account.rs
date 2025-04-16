9b550c08b76b2ee8d185030c003f6e5dc73630cb
Zhang ShengYan <yan.code@gmail.com>
2025-04-16 10:00:28 +0800

    feat: account

--- src/command/account.rs
+++ src/command/account.rs
@@ -7,29 +7,46 @@ use spl_associated_token_account::get_associated_token_address;
 
 impl Miner {
     pub async fn account(&self, args: AccountArgs) {
-        // if let Some(command) = args.command {
-        //     match command {
-        //         AccountCommand::Close(args) => {
-        //             self.close(args).await;
-        //         }
-        //     }
-        // } else {
-        //     self.get_account(args).await;
-        // }
+        if let Some(command) = args.command {
+            match command {
+                AccountCommand::Close(args) => self.close(args).await,
+            }
+        } else {
+            self.get_account(args).await;
+        }
     }
-    // async fn get_account(&self, args: AccountArgs) {
-    //     let signer = self.signer();
-    //     let address = match (&args.address, args.proof.is_some()) {
-    //         (Some(add_str), _) => {
-    //             Pubkey::from_str(add_str).map_err(|e| {
-    //                 println!("Invalid address: {}", e);
-    //                 std::process::exit(2);
-    //             });
-    //         }
-    //         (None, true) => self.get_proof_account(args).await,
-    //         (None, false) => signer.pubkey(),
-    //     };
-    // }
+
+    async fn get_account(&self, args: AccountArgs) {
+        // Parse account address
+        let signer = self.signer();
+        let address = if let Some(address) = &args.address {
+            if let Ok(address) = Pubkey::from_str(&address) {
+                address
+            } else {
+                println!("Invalid address: {:?}", address);
+                return;
+            }
+        } else if args.proof.is_some() {
+            return self.get_proof_account(args).await;
+        } else {
+            signer.pubkey()
+        };
+        // Aggregate data
+        let mut data = vec![];
+        self.get_account_data(address, &mut data).await;
+        self.get_proof_data(address, &mut data).await;
+
+        // Build table
+        let mut table = Table::new(data);
+        table.with(Remove::row(Rows::first()));
+        table.modify(Columns::single(1), Alignment::right());
+        table.with(Style::blank());
+        table.section_title(0, "Account");
+        table.section_title(3, "Proof");
+
+        println!("{table}\n");
+    }
+
     async fn get_account_data(&self, authority: Pubkey, data: &mut Vec<TableData>) {
         let token_account_address =
             get_associated_token_address(&authority, &eore_api::consts::MINT_ADDRESS);
@@ -62,5 +79,37 @@ impl Miner {
             key: "ETH".to_string(),
             value: format!("{} ETH", lamports_to_sol(sol_balance)),
         });
+    }
+
+    async fn get_proof_account(&self, args: AccountArgs) {
+        // Parse account address
+        let proof_address = if let Some(address) = &args.proof {
+            if let Ok(address) = Pubkey::from_str(&address) {
+                address
+            } else {
+                println!("Invalid address: {:?}", address);
+                return;
+            }
+        } else {
+            return;
+        };
+
+        // Aggregate data
+        let proof = get_proof(&self.rpc_client, proof_address)
+            .await
+            .expect("Failed to fetch proof account");
+        let mut data = vec![];
+        self.get_account_data(proof.authority, &mut data).await;
+        self.get_proof_data(proof.authority, &mut data).await;
+
+        // Build table
+        let mut table = Table::new(data);
+        table.with(Remove::row(Rows::first()));
+        table.modify(Columns::single(1), Alignment::right());
+        table.with(Style::blank());
+        table.section_title(0, "Account");
+        table.section_title(3, "Proof");
+
+        println!("{table}\n");
     }
 }
